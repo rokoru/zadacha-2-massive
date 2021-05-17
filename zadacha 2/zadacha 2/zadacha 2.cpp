2ac196dcd7eaadbf381d3e0f0e605d6e825b39f4
Dmitry <gomerty15@mail.ru>
2021-05-17 14:48:55 +0300

    добавленно решение

---
+++ zadacha 2/zadacha 2/zadacha 2.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+#include <cstdlib>
+
+
+int main()
+{
+    int* a = new int[100];
+    int* b = new int[50];
+
+    for (int i = 0; i < 100; ++i) {
+        a[i] = rand() % 30;
+    }
+
+    for (int j = 0; j < 50; ++j) {
+        b[j] = rand() % 20;
+    }
+    int s = 0;
+    for (int e = 0; e < 50; ++e) {
+        if (a[e] == b[e]) {
+            s += 1;
+        }
+        
+    }
+    std::cout << s << std::endl;
+}
